611f7ca224ae64e2224b677f18c536d74cc45830
Akash <akash.puranik06@gmail.com>
2024-11-04 22:10:07 +0530

    task1

---
+++ domManupulatonById.js
@@ -0,0 +1,17 @@
+// Write your code below:
+const main_heading = document.getElementById('main-heading');
+
+main_heading.textContent = 'Fruit World'
+main_heading.style.color = 'orange';
+
+const head = document.getElementById('header');
+
+head.style.backgroundColor = 'green';
+head.style.borderBottom = 'orange';
+
+const basket = document.getElementById('basket-heading');
+basket.style.color = 'green';
+
+const thanks = document.getElementById('thanks');
+thanks.innerHTML = '<p>Please visit us again</p>';
+
