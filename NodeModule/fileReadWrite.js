5c8721c7274e258808672adf41b523b04c8bb016
Akash <akash.puranik06@gmail.com>
2024-11-15 22:12:50 +0530

    clean up , made new route

--- NodeModule/fileReadWrite.js
+++ NodeModule/fileReadWrite.js
@@ -1,49 +1,7 @@
 const http = require('http');
-const fs = require('fs');
-const { on } = require('events');
+const route = require('./routes.js');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-
-    if (url === '/') {
-        fs.readFile('Message.txt', { encoding: 'utf-8' }, (err, data) => {
-            if (err) {
-                console.log(err)
-            }
-            res.setHeader('Content-Type', 'text/html');
-            res.write('<html>');
-            res.write(`<head><title></title></head>`)
-            res.write(`<body>${data}</body>`);
-            res.write(`<body><form action="/message" method="POST"><input type="text" name="message"></input><button type="submit">Click</button></form></body>`)
-            res.write('</html>');
-
-
-            return res.end();
-
-        })
-    }
-
-    else if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        });
-
-        return req.on('end', () => {
-
-            const parseBody = Buffer.concat(body).toString();
-            const message = parseBody.split('=')[1];
-            fs.writeFile('Message.txt', message, () => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-
-        })
-
-    }
-
-});
+console.log(route.sometext);
+const server = http.createServer(route.handle);
 
 server.listen(4000);
